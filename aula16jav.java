5df823739152f825e167d79efff8a8b410388dde
CARLA <101472271+Carlamaster@users.noreply.github.com>
2022-06-09 11:41:55 -0300

    Add files via upload

---
+++ aula16jav.java
@@ -0,0 +1,41 @@
+// Criação de aplicativos de sistemas, Aula 16
+// Aluna: Carla 
+
+
+import javax.swing.*;
+
+class aula16jav 
+{
+    public static void soma(int vet [])
+    {
+     int s=0;
+   
+     for (int i=0 ; i<vet.length; i++)
+             {
+                 s = s + vet[i];  
+             }
+    JOptionPane.showMessageDialog(null, "A somatoria e "+s);
+    }
+
+    public static int produto (int vet [])
+    {
+        int p=1;
+       for (int i=0 ; i<vet.length; i++)
+        {
+             p = p * vet[i];
+        }
+        return p;
+    }
+    
+    public static void main (String entrada [])
+    {
+        int s=0, vetor[] = {2,4,6,8,10};
+        int r;
+        
+        soma (vetor);
+        r = produto (vetor);
+        JOptionPane.showMessageDialog(null,"A produtoria e "+ r);
+       
+        System.exit(0);
+    }
+}
